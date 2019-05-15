34837846a6372f4d1c7a3c305c96d5ccc0ea6281
MrMorello <50555305+MrMorello@users.noreply.github.com>
2019-05-15 06:51:37 +0300

    proj

---
+++ order/models.py
@@ -0,0 +1,48 @@
+from django.db import models
+from products.models import Product
+
+
+class Status(models.Model):
+    name = models.CharField(max_length=24, blank=True, null=True, default=None)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return "Статус %s" % self.name
+
+    class Meta:
+        verbose_name = 'Статус заказа'
+        verbose_name_plural = 'Статусы заказа'
+
+
+class Order(models.Model):
+    customer_name = models.CharField(max_length=128, blank=True, null=True, default=None)
+    customer_email = models.EmailField(blank=True, null=True, default=None)
+    customer_phone = models.CharField(max_length=48, blank=True, null=True, default=None)
+    comments = models.TextField(blank=True, null=True, default=None)
+    status = models.ForeignKey(Status, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return "Заказ %s %s" % (self.id, self.status.name)
+
+    class Meta:
+        verbose_name = 'Заказ'
+        verbose_name_plural = 'Заказы'
+
+
+class ProductInOrder(models.Model):
+    order = models.ForeignKey(Order, blank=True, null=True, default=None, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, blank=True, null=True, default=None, on_delete=models.CASCADE)
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __str__(self):
+        return "%s" % self.product.name
+
+    class Meta:
+        verbose_name = 'товар'
+        verbose_name_plural = 'товары'
