277ce2f0db4f398f149a07a8cada079126533aac
MrMorello <50555305+MrMorello@users.noreply.github.com>
2019-09-17 09:00:54 +0300

    MFI

--- jsMFI.js
+++ jsMFI.js
@@ -1,4 +1,4 @@
-// Change inscription on "submit" button
+ï»¿// Change inscription on "submit" button
 function changeFunction() {
     var x = document.getElementById("chooseAction").value;
     document.getElementById("sendButton").innerHTML = x;
@@ -12,35 +12,41 @@ function deleteSpaces() {
 }
 
 // Chech equal of passwords
+var checkPasswd = true
 function check() {
     if (document.getElementById('password').value ==
         document.getElementById('confirm_password').value) {
         document.getElementById('message').style.color = 'green';
         document.getElementById('message').innerHTML = '&#10004';
-        var checkPasswd = true;
+        checkPasswd = true;
     } else {
         document.getElementById('message').style.color = 'red';
         document.getElementById('message').innerHTML = '&#10008';
-        var checkPasswd = false;
+        checkPasswd = false;
     }
 }
 
 // Check Age
+var checkAge16 = true;
 function checkAge() {
     var age = document.getElementById("age").value;
-    if (age >= 14 && age <= 120) {
-        var checkAge16 = true;
+    if (age >= 16 && age <= 120) {
+        checkAge16 = true;
     } else {
-        var checkAge16 = false;
+        checkAge16 = false;
     }
 }
 
-//var chBox = document.getElementById("checkBox").checked;
+//var checkboxes = document.getElementById('checkBox').checked;
+var checkboxes = document.getElementById('checkBox').value;
+console.log(checkboxes);
 
 function checkAll() {
+    console.log(checkboxes);
     if ((checkAge && checkPasswd) == true) {
-        alert("Âñå âåğíî")
+        alert("Thank you!");
     } else {
-        alert("Ïğîâåğüòå ïîëÿ")
+        alert("Invalid values");
     }
 }
+
