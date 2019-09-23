4e668e0b41c66e80b59b81770dd0ef84b576f0b9
MrMorello <50555305+MrMorello@users.noreply.github.com>
2019-09-23 16:23:17 +0300

    MFI

--- MergeSortSkeleton.cs
+++ MergeSortSkeleton.cs
@@ -36,15 +36,20 @@ namespace Myapp1
             for (int i = 0; i < mass1.Length + mass2.Length; i++)
             {
                 if (b < mass2.Length && a < mass1.Length)
+                {
                     if (mass1[a] > mass2[b] && b < mass2.Length)
+                    {
                         merged[i] = mass2[b++];
+                    }                        
                     else
+                    {
                         merged[i] = mass1[a++];
+                    }                        
+                }                    
+                else if (b < mass2.Length)
+                { merged[i] = mass2[b++]; }
                 else
-                    if (b < mass2.Length)
-                    merged[i] = mass2[b++];
-                else
-                    merged[i] = mass1[a++];
+                { merged[i] = mass1[a++]; }
             }
             return merged;
         }
