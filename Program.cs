6342cd390507085d37f930d013d25f07e478579b
MrMorello <50555305+MrMorello@users.noreply.github.com>
2019-09-23 21:41:47 +0300

    Lina's

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Myapp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите чисело элементов последовательности");
+            string elementCount = Console.ReadLine();
+
+            int elem_nums;
+            while (!Int32.TryParse(elementCount, out elem_nums))
+            {
+                Console.WriteLine("Введите чисело элементов последовательности");
+                elementCount = Console.ReadLine();
+            }
+
+            int[] mas = new int[elem_nums];
+            Console.WriteLine($"Введите {elem_nums} натуральных чисел. Здесь надо посмотреть Int32.Max ");
+            //здесь нужна проверка что число натуральное, что оно больше нуля и что оно меньше Int32.Max 
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write("{0}-е число: ", i + 1);
+                string elementString = Console.ReadLine();
+                int element;
+                while(!Int32.TryParse(elementString, out element))
+                {
+                    Console.WriteLine("Введите число. {0}-е число: ", i + 1);
+                    elementString = Console.ReadLine();
+                }
+
+                mas[i] = element; 
+            }
+
+            Sorter sorter = new Sorter();
+
+            Console.WriteLine(string.Join(",", sorter.Sort(mas)));
+            Console.ReadKey();
+
+        }
+
+    }
+}
---
+++ Sorter.cs
@@ -0,0 +1,56 @@
+﻿using System.Linq;
+
+namespace Myapp1
+{
+    public class Sorter
+    {
+        public Sorter()
+        {
+
+        }
+
+        public int[] Sort(int[] massive)
+        {
+            int[] tempMassive;
+            if (massive.Length == 1)
+            {
+                tempMassive = massive;
+            }
+            else
+            {
+                int mid_point = massive.Length / 2;
+                tempMassive = Merge(Sort(massive.Take(mid_point).ToArray()), Sort(massive.Skip(mid_point).ToArray()));
+            }
+
+            return tempMassive;
+        }
+        public int[] Merge(int[] mass1, int[] mass2)
+        {
+            int a = 0, b = 0;
+            int[] merged = new int[mass1.Length + mass2.Length];
+            for (int i = 0; i < mass1.Length + mass2.Length; i++)
+            {
+                if (b < mass2.Length && a < mass1.Length)
+                {
+                    if (mass1[a] > mass2[b] && b < mass2.Length)
+                    {
+                        merged[i] = mass2[b++];
+                    }
+                    else
+                    {
+                        merged[i] = mass1[a++];
+                    }
+                }
+                else if (b < mass2.Length)
+                {
+                    merged[i] = mass2[b++];
+                }
+                else
+                {
+                    merged[i] = mass1[a++];
+                }
+            }
+            return merged;
+        }
+    }
+}
